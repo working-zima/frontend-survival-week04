901b89d8bb4a5124bb155180350a03a21cbaff0d
working-zima <working.zima@gmail.com>
2024-02-21 21:18:22 +0900

    feat: update backend code

--- express-app/app.ts
+++ express-app/app.ts
@@ -1,1 +1,24 @@
-// TODO: Express 를 이용하여 서버를 만들어 주세요.
+import express from 'express';
+import cors from 'cors';
+
+import { restaurants } from './restaurants.json';
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/restaurants', (req, res) => {
+  res.status(200).send(restaurants);
+});
+
+app.post('/orders', (req, res) => {
+  const post = req.body;
+  res.status(201).send({ id: Date.now().toString(), ...post });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
