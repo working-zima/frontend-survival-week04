1cb18484662c43c6508e395d515d57a39726c148
working-zima <working.zima@gmail.com>
2024-02-23 21:49:23 +0900

    style: update receipt design

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -8,6 +8,7 @@ export default function App() {
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
   const [filterText, setFilterText] = useState<string>('');
   const [filterCategory, setFilterCategory] = useState<string>('전체');
+  const [isOrdered, setIsOrdered] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,7 +30,35 @@ export default function App() {
         setFilterText={setFilterText}
         setFilterCategory={setFilterCategory}
       />
-      <p>[영수증 나오는 곳]</p>
+      {isOrdered ? (
+        <div
+          style={{
+            width: '50%',
+            border: '1px solid black',
+            textAlign: 'center',
+          }}
+        >
+          <h2>영수증</h2>
+          <div>
+            <h3>주문번호</h3>
+            <p>12345913413</p>
+          </div>
+          <div>
+            <h3>주문목록</h3>
+            <ul style={{ padding: '0px', listStyle: 'none' }}>
+              <li style={{ display: 'flex', paddingBlock: '0.5rem' }}>
+                <span style={{ margin: '0px auto' }}>메뉴</span>
+              </li>
+              <li style={{ display: 'flex', paddingBlock: '0.5rem' }}>
+                <span style={{ margin: '0px auto' }}>하나 더</span>
+              </li>
+            </ul>
+          </div>
+          <p>총 가격: 3,500원</p>
+        </div>
+      ) : (
+        <p>[영수증 나오는 곳]</p>
+      )}
     </div>
   );
 }
